fce3ee7d63c8686253352a3360318144ede34746
yiweishen1988 <yiweishen1988@gmail.com>
2019-11-04 22:37:35 +0100

    Refactoring the project

---
+++ Graph/Graph.Base/Entities/AdjacencyList.cs
@@ -0,0 +1,49 @@
+﻿namespace Graph.Base
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class AdjacencyList<T>
+    {
+        public int V { get; set; }
+        public List<T>[] G { get; set; }
+
+        public AdjacencyList(int v)
+        {
+            V = v;
+            G = new List<T>[V];
+
+            for (var i = 0; i < G.Length; i++)
+            {
+                G[i]=new List<T>();
+            }
+        }
+
+        public List<T> this[int index]
+        {
+            get
+            {
+                return G[index];
+            }
+            set
+            {
+                G[index] = value;
+            }
+        }
+
+        public void DisplayGraphDFS(int start)
+        {
+            Console.Write(start+" ");
+            Visited[start] = true;
+
+            foreach (int i in G[start])
+            {
+                if (Visited[i] != true)
+                {
+                    DisplayGraphDFS(i);
+                    Visited[i] = true;
+                }
+            }
+        }
+    }
+}
---
+++ Graph/Graph.Base/Helper/BuilderHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Graph.Base.Helper
+{
+    using System.Collections.Generic;
+
+    public static class BuilderHelper
+    {
+        public static List<T>[] Build<T>(int v)
+        {
+
+            return new AdjacencyList<T>(v);
+        }
+
+        public static void AddEdge<T>(this AdjacencyList<T> graph, int index, T value)
+        {
+            graph[index].Add(value);
+        }
+    }
+}
