8afcc6ddfea4772a7d56a92542ce3c50a6d1da0e
yiweishen1988 <yiweishen1988@gmail.com>
2020-03-20 18:20:08 +0100

    Hamiltonian Path

--- Graph/Graph.ShortestPath/HamiltonianPath.cs
+++ Graph/Graph.ShortestPath/HamiltonianPath.cs
@@ -1,135 +1,126 @@
-﻿using System;
-using System.Collections.Generic;
-using Collection;
+﻿using System.Collections.Generic;
 
 namespace Graph.ShortestPath
 {
     public class HamiltonianPath
     {
-        public static int[,] Graph { get; set; }
-        public static int V { get; set; }
-        public static int[] Ps;
-        public static int Src;
-        public static bool Found = false;
-
-        public static void GetPath(int[,] graph)
+        public static List<int> GetHamiltonianPath(List<int>[] graph)
         {
-            Graph = graph;
-            V = Graph.GetLength(0);
-            Ps = new int[V];
+            int v = graph.Length;
 
-            for (int i = 0; i < V; i++)
+            bool found = false;
+            var childs = new int[v];
+            int src = -1;
+
+            for (int i = 0; i < v; i++)
             {
-                bool[] vs = new bool[V];
-                int[] ps = new int[V];
-                for (int h = 0; h< V; h++)
+                var vs = new bool[v];
+                
+                for (int h = 0; h < v; h++)
                 {
-                    ps[h] = -1;
+                    childs[h] = -1;
                 }
-                vs[i] = true;
-                if (DfsPathUntil(vs, i, 1, ps))
+
+                if (DfsPathUntil(graph, v, vs, i, 1, childs))
                 {
-                    Found = true;
-                    ps.CopyTo(Ps, 0);
-                    Src = i;
+                    found = true;
+                    src = i;
                     break;
                 }
             }
 
-            if (Found)
+            if (found)
             {
-                int t = Src;
-                Console.Write(t);
-                while (Ps[t] != -1)
+                var res = new List<int>();
+                res.Add(src);
+                while (childs[src] != -1)
                 {
-                    Console.Write("=>" + Ps[t]);
-                    t = Ps[t];
+                    src = childs[src];
+                    res.Add(src);
                 }
 
+                return res;
             }
             else
             {
-                Console.WriteLine("no");
+                return null;
             }
         }
 
-        public static bool DfsPathUntil(bool[] vs, int src, int level, int[] ps)
+        public static List<int> GetHamiltonianCircle(List<int>[] graph)
         {
-            if (level == V) return true;
+            int v = graph.Length;
+            var childs = new int[v];
+            var vs = new bool[v];
+            int src = 0;
 
-            for (int i = 0; i < V; i++)
+            for (int h = 0; h < v; h++)
             {
-                if (!vs[i] && Graph[src, i] == 1)
+                childs[h] = -1;
+            }
+
+            if (DfsCircleUntil(graph, v, vs, src, 1, childs))
+            {
+                var res = new List<int>();
+                res.Add(src);
+                while (childs[src] != -1)
                 {
-                    vs[i] = true;
-                    ps[src] = i;
-                    if (DfsPathUntil(vs, i, level + 1, ps))
-                    {
-                        return true;
-                    }
-                    vs[i] = false;
-                    ps[src] = -1;
-                }
-            }
-
-            return false;
-        }
-
-        public static void GetCircle(int[,] graph)
-        {
-            Graph = graph;
-            V = Graph.GetLength(0);
-            Ps = new int[V];
-
-            bool[] vs = new bool[V];
-            for (int h = 0; h < V; h++)
-            {
-                Ps[h] = -1;
-            }
-            vs[0] = true;
-            if (DfsCircleUtil(vs, 0, 1, Ps))
-            {
-                Found = true;
-                Src = 0;
-            }
-
-            if (Found)
-            {
-                int t = Src;
-                Console.Write(t);
-                while (Ps[t] != -1)
-                {
-                    Console.Write("=>" + Ps[t]);
-                    t = Ps[t];
+                    src = childs[src];
+                    res.Add(src);
                 }
 
+                return res;
             }
             else
             {
-                Console.WriteLine("no");
+                return null;
             }
         }
 
-        public static bool DfsCircleUtil(bool[] vs, int src, int level, int[] ps)
+        static bool DfsPathUntil(List<int>[] graph, int v, bool[] vs, int src, int level, int[] childs)
         {
-            if (level == V && Graph[src, Src] == 1) return true;
+            if (level == v) return true;
 
-            for (int i = 0; i < V; i++)
+            vs[src] = true;
+
+            foreach (var c in graph[src])
             {
-                if (!vs[i] && Graph[src, i] == 1)
+                if (!vs[c])
                 {
-                    vs[i] = true;
-                    ps[src] = i;
-                    if (DfsCircleUtil(vs, i, level + 1, ps))
+                    childs[src] = c;
+                    if (DfsPathUntil(graph, v, vs, c, level + 1, childs))
                     {
                         return true;
                     }
-                    vs[i] = false;
-                    ps[src] = -1;
+
+                    childs[src] = -1;
                 }
             }
 
-            return false;
+            return vs[src] = false;
+        }
+
+        static bool DfsCircleUntil(List<int>[] graph, int v, bool[] vs, int src, int level, int[] childs)
+        {
+            if (level == v && graph[src].Contains(0)) return true;
+
+            vs[src] = true;
+
+            foreach (var c in graph[src])
+            {
+                if (!vs[c])
+                {
+                    childs[src] = c;
+                    if (DfsPathUntil(graph, v, vs, c, level + 1, childs))
+                    {
+                        return true;
+                    }
+
+                    childs[src] = -1;
+                }
+            }
+
+            return vs[src] = false;
         }
     }
 }
