b45c79abfb07dcf5a729665f37d0eae76640c994
yiweishen1988 <yiweishen1988@gmail.com>
2020-04-21 23:02:32 +0200

    Pattern Searching

---
+++ PatternSearching/ClosestPalindrome.cs
@@ -0,0 +1,86 @@
+﻿namespace StringProblem
+{
+    using DataStructure.Graph.Heap;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ClosestPalindrome
+    {
+        // Given a number N.our task is to find the closest Palindrome number
+        // whose absolute difference with given number is minimum and absolute 
+        // difference must be greater than 0.
+
+        public static string GetClosestPalindromeString(string str)
+        {
+            long rN = long.Parse(str);
+            long len = str.Length;
+
+            //number like 1000 expected 999
+            if (len == 1 || (rN == (long)Math.Pow(10, len - 1)))
+            {
+                return (rN - 1).ToString();
+            }
+
+            //number like 999 expected 1001
+            if (len>2 && rN == (long)Math.Pow(10, len)-1)
+            {
+                return (rN + 2).ToString();
+            }
+
+            var pQ = new PriorityQueue<long>((f, n) => {
+                long v1 = Math.Abs(f - rN);
+                long v2 = Math.Abs(n - rN);
+                if (v1 == v2)
+                {
+                    return Math.Sign(f - n);
+                }
+                else
+                {
+                    return Math.Sign(v1 - v2);
+                }
+            });
+
+            int l = str.Length / 2;
+            int pl = str.Length % 2 == 0 ? l : l + 1;
+
+            var pStr = str.Substring(0, pl);
+            var list = GetPossibleValues(pStr);
+            long value = long.Parse(pStr);
+            list.AddRange(GetPossibleValues((value+1).ToString()));
+
+            long valueR1 = value - 1;
+            if (valueR1 == 0)
+            {
+                valueR1 = 9;
+            }
+
+            list.AddRange(GetPossibleValues(valueR1.ToString()));
+            list.Remove(rN);
+
+            foreach (var item in list)
+            {
+                pQ.Enqueue(item);
+            }
+
+            return pQ.Peek().ToString();
+        }
+
+        static List<long> GetPossibleValues(string root)
+        {
+            var list = new List<long>();
+            int len = root.Length;
+            long value = long.Parse(root);
+            list.Add(value);
+            long rValue = long.Parse(new string(root.Reverse().ToArray()));
+            list.Add(value * (long)Math.Pow(10, len) + rValue);
+
+            if (len > 1)
+            {
+                list.Add(value * (long)Math.Pow(10, len-1) + long.Parse(new string(root.Substring(0, len - 1).Reverse().ToArray())));
+            }
+
+            return list;
+        }
+    }
+}
---
+++ PatternSearching/KMPSearching.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+
+namespace PatternSearching
+{
+    public class KMPSearching
+    {
+        /// <summary>
+        /// Given a text txt and a pattern string pattern, write a function 
+        /// to get all occurrences of pattern in txt. 
+        /// You may assume that n > m.
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <param name="text"></param>
+        
+        public static IList<int> SearchPattern(string pattern, string text)
+        {
+            int[] lpsArray;
+
+            CalculateLps(pattern, out lpsArray);
+
+            var result = new List<int>();
+            int j = 0;
+            for (int i = 0; i < text.Length;)
+            {
+                if(text[i] == pattern[j])
+                {
+                    i++;
+                    j++;
+
+                    if(j == lpsArray.Length)
+                    {
+                        result.Add(i - j);
+                        j = lpsArray[j - 1];
+                    }
+
+                }else
+                {
+                    if (j > 0)
+                    {
+                        j = lpsArray[j - 1];
+                    }else
+                    {
+                        i++;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static void CalculateLps(string pattern, out int[] lps)
+        {
+            int len = 0;
+            char[] chars = pattern.ToCharArray();
+            lps = new int[chars.Length];
+
+            for (int i = 1; i < chars.Length;)
+            {
+                if (chars[i] == chars[len])
+                {
+                    lps[i] = len + 1;
+                    len++;
+                    i++;
+                }
+                else if (chars[i] != chars[len] && len > 0)
+                {
+                    len = lps[len - 1];
+                }
+                else
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+}
