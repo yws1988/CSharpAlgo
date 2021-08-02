5fcd3200a89029389c343afe7d4b6328681ed9a3
yws1988 <yiweishen1988@gmail.com>
2021-08-02 23:06:16 +0200

    Add the Binary Search Tree

---
+++ Exercice/Excercises/Graph/Tree/ShortestPathInInfiniteKTree.cs
@@ -0,0 +1,124 @@
+﻿/*
+You're given a K-ary infinite tree rooted at a vertex numbered 1. All its edges are weighted 1 initially.
+Any node  will have exactly  children numbered as:
+K*X, K*X+1, ..., K*X+K-1
+You are given q queries to answer which will be of the following two types:
+
+: Print the shortest distance between nodes u and v.
+: Increase the weight of all edges on the shortest path between u and v by w.
+
+For each query of type 1 u v, print a single integer denoting the shortest distance between u and v.
+*/
+
+namespace CSharpAlgo.Excercise.Excercises.Graph.Tree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ShortestPathInInfiniteKTree
+    {
+        public static int k, q;
+        public static long[] ns;
+        public static Dictionary<(long, long), long> weights;
+
+        public static void Solve()
+        {
+            weights = new Dictionary<(long, long), long>();
+
+            for (int i = 0; i < q; i++)
+            {
+                long qt = long.Parse(Console.ReadLine());
+                long u = long.Parse(Console.ReadLine());
+                long v = long.Parse(Console.ReadLine());
+
+                long lca = GetLowestCommonAncestor(u, v);
+
+                if (qt == 1)
+                {
+                    Console.WriteLine(GetCosts(lca, u) + GetCosts(lca, v));
+                }
+                else
+                {
+                    long w = ns[3];
+
+                    UpdateCosts(lca, u, w);
+                    UpdateCosts(lca, v, w);
+                }
+            }
+        }
+
+        static long GetCosts(long parent, long child)
+        {
+            if (parent == child) return 0;
+
+            long cost = 0;
+
+            while (child != parent)
+            {
+                long cparent = child / k;
+                var path = (cparent, child);
+                if (weights.ContainsKey(path))
+                {
+                    cost += weights[path];
+                }
+                else
+                {
+                    weights[path] = 1;
+                    cost += weights[path];
+                }
+
+                child = cparent;
+            }
+
+            return cost;
+        }
+
+        static void UpdateCosts(long parent, long child, long weight)
+        {
+            if (parent == child) return;
+
+            while (child != parent)
+            {
+                long cparent = child / k;
+                var path = (cparent, child);
+
+                if (!weights.ContainsKey(path))
+                {
+                    weights[path] = 1;
+                }
+
+                weights[path] += weight;
+
+                child = cparent;
+            }
+        }
+
+        static long GetLowestCommonAncestor(long u, long v)
+        {
+            long max = Math.Max(u, v);
+            long min = Math.Min(u, v);
+
+            int maxd = (int)Math.Log(max, k);
+            int mind = (int)Math.Log(min, k);
+
+            while (maxd != mind)
+            {
+                max = max / k;
+                maxd--;
+            }
+
+            if (max == min)
+            {
+                return max;
+            }
+
+            while (max != min)
+            {
+                max /= k;
+                min /= k;
+            }
+
+            return max;
+        }
+    }
+}
---
+++ Exercice/HackerEarth/Graph/Graph.Tree/PopulationOutburst.cs
@@ -0,0 +1,106 @@
+﻿namespace CSharpAlgo.Excercise.HackerEarth.Graph.Graph.Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class PopulationOutburst
+    {
+        public static int n, rc0;
+
+        public static void Solve()
+        {
+            var tmp = ReadIntArray();
+            n = tmp[0];
+            rc0 = tmp[1];
+
+            var queue = new Queue<Node>();
+            queue.Enqueue(new Node(0, -1, 0, 1, rc0));
+            
+            int numOfElement = 1;
+
+            while (queue.Count() > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node.Parent != -1)
+                {
+                    Console.WriteLine($"{node.Parent} {node.Level} {node.Sibling}");
+                }
+
+                int sibling = 1;
+                while (sibling <= node.Capacity && numOfElement <= n)
+                {
+                    var temp = ReadIntArray();
+                    queue.Enqueue(new Node(temp[0], node.Num, node.Level + 1, sibling, temp[1]));
+                    sibling++;
+                    numOfElement++;
+                }
+            }
+        }
+
+        #region Main
+
+        public static TextReader Reader;
+        static void MainF(string[] args)
+        {
+#if false
+            Reader = new StreamReader(@"test\test.txt");
+#else
+            Reader = new StreamReader(Console.OpenStandardInput());
+#endif
+            Solve();
+            Reader.Close();
+        }
+
+        #endregion
+
+        private static Queue<string> currentLineTokens = new Queue<string>();
+        public static string ReadToken() { while (currentLineTokens.Count == 0) currentLineTokens = new Queue<string>(ReadStringArray()); return currentLineTokens.Dequeue(); }
+        private static string[] ReadAndSplitLine() { return Reader.ReadLine().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries); }
+        public static int ReadInt() { return int.Parse(ReadToken()); }
+        public static int[] ReadIntArray() { return ReadStringArray().Select(int.Parse).ToArray(); }
+        public static long[] ReadLongArray() { return ReadAndSplitLine().Select(long.Parse).ToArray(); }
+        public static string ReadString() { return Reader.ReadLine(); }
+        public static string[] ReadStringArray() { return Reader.ReadLine().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries); }
+        public static string[] ReadLines(int quantity) { string[] lines = new string[quantity]; for (int i = 0; i < quantity; i++) lines[i] = Reader.ReadLine().Trim(); return lines; }
+
+        public static List<int>[] CreateListArray(int n, int[][] arr, bool isDirected = false)
+        {
+            var graph = Enumerable.Range(0, n).Select(s => new List<int>()).ToArray();
+            foreach (var item in arr)
+            {
+                int src = item[0];
+                int des = item[1];
+                graph[src].Add(des);
+
+                if (!isDirected)
+                {
+                    graph[des].Add(src);
+                }
+            }
+
+            return graph;
+        }
+
+        public class Node
+        {
+            public int Num { get; set; }
+            public int Parent { get; set; }
+            public int Level { get; set; }
+            public int Sibling { get; set; }
+            public int Capacity { get; set; }
+
+            public Node(int num, int parent, int level, int sibling, int capacity)
+            {
+                Num = num;
+                Parent = parent;
+                Level = level;
+                Sibling = sibling;
+                Capacity = capacity;
+            }
+        }
+    }
+
+}
